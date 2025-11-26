879eebfccf997089070053f7f5f6982a70e83c49
mohamed <mohamed7040559@gmail.com>
2025-11-26 15:45:30 +0100

    Eingabe für höhe und Breite mit Fehler meldung bis die eingabe gültig ist und erstellung der Karte mit den wände nur

--- RDG.cs
+++ RDG.cs
@@ -2,22 +2,85 @@
 
 public class RDG
 {
-	public RDG()
-	{
-		const char WAND = '#';      // Wand symbol
+    const char WAND = '#';   // Wand-Symbol
+    const char GANG = '.';   // Gang-Symbol
+    const char START = 'S';  // Start
+    const char ENDE = 'E';   // Ende
 
-        static char[,] InitialisireKarte(int breite, int hoehe)     // Methode zur Initialisierung der Karte
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Wie groß soll die Höhe des Dungeons sein (min 10 & max 25)?");
+        int hoehe = Convert.ToInt32(Console.ReadLine());
+        while (true)
         {
-			char[,] Karte = new char[hoehe, breite];        // 2D-Array für die Karte erstellen
-
-            for (int y = 0; y < hoehe; y++)     // Äusserste Schleife für die Höhe der Karte(y)
+            try
             {
-				for (int x = 0; x < breite; x++)        // Innere Schleife für die Breite der Karte(x)
+                breite = Convert.ToInt32(Console.ReadLine());
+                if (hoehe < 10 || hoehe > 25)
                 {
-					Karte[x, y] = WAND;     // Jede Position der Karte mit dem Wand-Symbol füllen
+                    Console.WriteLine("Die Höhe darf nicht kleiner als 10 oder Höhe als 25 sein. \nVersuchen Sie es erneut:");
+                }
+                else
+                {
+                    break;
                 }
             }
-			return Karte;       // Karte zurückgeben
+            catch
+            {
+                Console.WriteLine("Fehler! Bitte geben Sie eine Zahl ein:");
+            }
+        }
+
+        Console.WriteLine("Wie groß soll die Breite des Dungeons sein (min 10 & max 50)?");
+        int breite;
+        while (true)
+        {
+            try
+            {
+                breite = Convert.ToInt32(Console.ReadLine());
+                if (breite < 10 || breite > 50)
+                {
+                    Console.WriteLine("Die Breite darf nicht kleiner als 10 oder größer als 50 sein. \nVersuchen Sie es erneut:");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Fehler! Bitte geben Sie eine Zahl ein:");
+            }
+        }
+
+        Random zufaelig = new Random();
+
+        char[,] karte = InitialisiereKarte(breite, hoehe);
+
+        // Karte ausgeben
+        for (int y = 0; y < hoehe; y++)
+        {
+            for (int x = 0; x < breite; x++)
+            {
+                Console.Write(karte[y, x]);
+            }
+            Console.WriteLine();
         }
     }
+
+    static char[,] InitialisiereKarte(int breite, int hoehe)
+    {
+        char[,] karte = new char[hoehe, breite];
+
+        for (int y = 0; y < hoehe; y++)
+        {
+            for (int x = 0; x < breite; x++)
+            {
+                karte[y, x] = WAND; // Jede Position mit Wand füllen
+            }
+        }
+
+        return karte;
+    }
 }
+
