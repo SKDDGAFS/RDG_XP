379babfc73f0b772b9259c9ae28635c72ace0317
MGhanem <mohamed7040559@gmail.com>
2025-12-01 11:46:59 +0100

    neu

--- RDG.cs
+++ RDG.cs
@@ -2,46 +2,27 @@
 
 public class RDG
 {
-    // Konstanten für die Symbole im Dungeon (gemäß Pflichtenheft)
-    const char WAND = '#';    // Wand-Symbol
-    const char GANG = '.';    // Gang-Symbol
-    const char START = 'S';   // Start-Symbol
-    const char ENDE = 'E';    // Ende-Symbol
+    const char WAND = '#';   // Wand-Symbol
+    const char GANG = '.';   // Gang-Symbol
+    const char START = 'S';  // Start
+    const char ENDE = 'E';   // Ende
 
     static void Main(string[] args)
     {
-        // ===========================================
-        //  DIMENSIONEN EINLESEN UND VALIDIEREN
-        // ===========================================
-
-        // --- HÖHE abfragen (min 10 & max 25) ---
         Console.WriteLine("Wie groß soll die Höhe des Dungeons sein (min 10 & max 25)?");
         int hoehe;
-        while (true) // Endlosschleife zur Wiederholung bei ungültiger Eingabe
+        while (true)
         {
             try
             {
-                // Liest Eingabe und versucht, diese in Zahl umzuwandeln
                 hoehe = Convert.ToInt32(Console.ReadLine());
-
-                // Prüft, ob die Zahl im zulässigen Bereich liegt
                 if (hoehe < 10 || hoehe > 25)
-                {
-                    Console.WriteLine("Die Höhe darf nicht kleiner als 10 oder Höhe als 25 sein. \nVersuchen Sie es erneut:");
-                }
-                else
-                {
-                    break; // Gültige Eingabe: Schleife verlassen
-                }
+                    Console.WriteLine("Die Höhe darf nicht kleiner als 10 oder größer als 25 sein. \nVersuchen Sie es erneut:");
+                else break;
             }
-            catch
-            {
-                // Fehler, wenn Eingabe keine Zahl war
-                Console.WriteLine("Fehler! Bitte geben Sie eine Zahl ein:");
-            }
+            catch { Console.WriteLine("Fehler! Bitte geben Sie eine Zahl ein:"); }
         }
 
-        // --- BREITE abfragen (min 10 & max 50) ---
         Console.WriteLine("Wie groß soll die Breite des Dungeons sein (min 10 & max 50)?");
         int breite;
         while (true)
@@ -50,86 +31,108 @@ public class RDG
             {
                 breite = Convert.ToInt32(Console.ReadLine());
                 if (breite < 10 || breite > 50)
-                {
                     Console.WriteLine("Die Breite darf nicht kleiner als 10 oder größer als 50 sein. \nVersuchen Sie es erneut:");
-                }
-                else
-                {
-                    break; // Gültige Eingabe: Schleife verlassen
-                }
+                else break;
             }
-            catch
-            {
-                Console.WriteLine("Fehler! Bitte geben Sie eine Zahl ein:");
-            }
+            catch { Console.WriteLine("Fehler! Bitte geben Sie eine Zahl ein:"); }
         }
 
-        // ===========================================
-        //  INITIALISIERUNG
-        // ===========================================
-
-        // Zentrales Objekt zur Generierung von Zufallszahlen
-        Random zufaellig = new Random();
-
-        // Erstellt das 2D-Array (char[,]) und füllt es komplett mit Wänden ('#')
         char[,] karte = InitialisiereKarte(breite, hoehe);
+        GaengeErstellen(karte, breite, hoehe, out int startX, out int startY, out int endX, out int endY);
 
-        // ===========================================
-        //  ZUFÄLLIGE GANG-PLATZIERUNG (30% Zufallslöcher)
-        // ===========================================
+        ErzeugeHauptWeg(karte, startX, startY, endX, endY, breite, hoehe);
+        ErzeugeNebenWege(karte, 20, breite, hoehe);
 
-        int total = hoehe * breite;
-        int gangAnzahl = total * 30 / 100; // Berechnet 30% der Gesamtfläche
-
-        if (gangAnzahl > 0)
-        {
-            // Erstellt ein 1D-Array mit allen möglichen Indizes (0 bis total-1)
-            int[] indices = new int[total];
-            for (int i = 0; i < total; i++)
-            {
-                indices[i] = i;
-            }
-
-            // Schleife wählt genau 'gangAnzahl' Zellen zufällig aus (Fisher-Yates Shuffle)
-            for (int i = 0; i < gangAnzahl; i++)
-            {
-                // Wählt eine zufällige Position 'j' im Rest des 'indices'-Arrays
-                int j = zufaellig.Next(i, total);
-
-                // --- Tausch (Swap) der Werte an Position i und j ---
-                int temp = indices[i];
-                indices[i] = indices[j];
-                indices[j] = temp;
-
-                // --- Umrechnung von 1D-Index in 2D-Koordinate (y, x) ---
-                int index = indices[i];
-                int y = index / breite;   // Zeile (Y) durch Ganzzahl-Division
-                int x = index % breite;   // Spalte (X) durch Modulo (Rest)
-
-                // Setzt die Zelle im 2D-Gitter auf Gang ('.')
-                karte[y, x] = GANG;
-            }
-        }
-
-        
-    }
-
-    // Erstellt das 2D-Array (char[,]) und füllt es vollständig mit Wänden ('#').
-    static char[,] InitialisiereKarte(int breite, int hoehe)
-    {
-        // Erstellung des 2D-Arrays: [Zeilen (y), Spalten (x)]
-        char[,] karte = new char[hoehe, breite];
-
-        // Durchlaufen jeder Zelle des Gitters
+        // Karte ausgeben
         for (int y = 0; y < hoehe; y++)
         {
             for (int x = 0; x < breite; x++)
             {
-                karte[y, x] = WAND; // Jede Position mit Wand füllen
-            }
-        }
+                char symbol = karte[y, x];
+                if (symbol == START) Console.ForegroundColor = ConsoleColor.Green;
+                else if (symbol == ENDE) Console.ForegroundColor = ConsoleColor.Red;
+                else Console.ResetColor();
 
+                Console.Write(symbol);
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+
+    static char[,] InitialisiereKarte(int breite, int hoehe)
+    {
+        char[,] karte = new char[hoehe, breite];
+        for (int y = 0; y < hoehe; y++)
+            for (int x = 0; x < breite; x++)
+                karte[y, x] = WAND;
         return karte;
     }
 
+    static void GaengeErstellen(char[,] karte, int breite, int hoehe, out int startX, out int startY, out int endX, out int endY)
+    {
+        Random zufaelig = new Random();
+        startY = zufaelig.Next(1, hoehe - 1);
+        startX = zufaelig.Next(1, breite - 1);
+        karte[startY, startX] = START;
+
+        do
+        {
+            endY = zufaelig.Next(1, hoehe - 1);
+            endX = zufaelig.Next(1, breite - 1);
+        } while (endY == startY && endX == startX);
+        karte[endY, endX] = ENDE;
+    }
+
+    static void ErzeugeHauptWeg(char[,] karte, int startX, int startY, int endX, int endY, int breite, int hoehe)
+    {
+        int x = startX, y = startY;
+        Random rnd = new Random();
+
+        while (x != endX || y != endY)
+        {
+            if (karte[y, x] != START && karte[y, x] != ENDE)
+                karte[y, x] = GANG;
+
+            if (rnd.Next(0, 100) < 70)
+            {
+                if (x < endX) x++;
+                else if (x > endX) x--;
+                if (y < endY) y++;
+                else if (y > endY) y--;
+            }
+            else
+            {
+                int r = rnd.Next(0, 4);
+                if (r == 0 && x < breite - 2) x++;
+                else if (r == 1 && x > 1) x--;
+                else if (r == 2 && y < hoehe - 2) y++;
+                else if (r == 3 && y > 1) y--;
+            }
+        }
+        karte[endY, endX] = ENDE;
+    }
+
+    static void ErzeugeNebenWege(char[,] karte, int menge, int breite, int hoehe)
+    {
+        Random rnd = new Random();
+        for (int i = 0; i < menge; i++)
+        {
+            int x = rnd.Next(1, breite - 1);
+            int y = rnd.Next(1, hoehe - 1);
+            if (karte[y, x] != WAND) continue;
+
+            int laenge = rnd.Next(10, 40);
+            for (int s = 0; s < laenge; s++)
+            {
+                karte[y, x] = GANG;
+                int r = rnd.Next(0, 4);
+                if (r == 0 && x < breite - 2) x++;
+                else if (r == 1 && x > 1) x--;
+                else if (r == 2 && y < hoehe - 2) y++;
+                else if (r == 3 && y > 1) y--;
+            }
+        }
+    }
 }
